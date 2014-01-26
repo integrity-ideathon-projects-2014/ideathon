f9e0e94f5e3038473ae485553d2abb050ed4aecc
Nabin Khadka <nbn_nbn_khadka@hotmail.com>
2014-01-26 10:19:07 +0545

    changes

--- src/com/example/ideathon/MainActivity.java
+++ src/com/example/ideathon/MainActivity.java
@@ -1,15 +1,30 @@
 package com.example.ideathon;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
+	Button start;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		start = (Button) findViewById(R.id.start);
+		start.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				//Intent intent = new Intent(this, );
+				
+			}
+		});
+		
 	}
 
 	@Override
